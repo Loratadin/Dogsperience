3a1b56badb3636943ed4a04e71d3a9b1ff37fc04
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-02-20 20:34:15 -0600

    Add my own slider

--- js/slider.js
+++ js/slider.js
@@ -21,3 +21,24 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += "active";
 }
+//test slider
+var i=0; // start point
+var images=[];
+var time=3000;
+//image list
+images[0]='./images/slider1.jpg';
+images[1]='./images/slider2.jpg';
+images[2]='./images/slider3.jpg';
+//change image
+function changeImg(){
+    document.slide.src=images[i];
+    if(i < images.length - 1){
+        i++;
+    }
+    else{
+        i=0;
+    }
+    setTimeout("changeImg()", time);
+}
+window.onload = changeImg;
+
