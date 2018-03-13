29a58f00b7d1a4a8dfa210abeeac4993534202e3
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-03-13 11:00:33 -0500

    Add mobile menu styles

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ $(".hamburger").click(function() {
     $(this).toggleClass("is-active");
     $(".mobile-menu").toggle();
 });
+$(".mobile-dropdown").click(function() {
+    $(".mobile-dropdown-content").toggleClass("is-active")
+});
